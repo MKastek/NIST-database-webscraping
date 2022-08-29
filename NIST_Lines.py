df9287f2921406c3f6890c561acf97f933d3ffbf
MKastek <mkastek8@gmail.com>
2022-08-29 13:21:55 +0200

    added new features

--- NIST_Lines.py
+++ NIST_Lines.py
@@ -10,10 +10,9 @@ from pandas.core.common import SettingWithCopyWarning
 warnings.simplefilter(action="ignore", category=SettingWithCopyWarning)
 
 
+class NIST_line():
 
-class Lines_LIBS(object):
-
-    def __init__(self, element, low_w, upper_w, strongLines=True, first_sp=True):
+    def __init__(self, element, low_w, upper_w, sp_num ,strongLines=True):
 
         self.element = element
         self.low_w = low_w
@@ -29,26 +28,27 @@ class Lines_LIBS(object):
 
         self.filter_nan_values()
         self.reset_index()
-        self.filter_columns()
 
-        if first_sp:
-            self.filter_sp()
+        self.filter_sp(sp_num)
 
     def retrieve_data(self):
-        site = "https://physics.nist.gov/cgi-bin/ASD/lines1.pl?spectra={}&limits_type=0&low_w={}&upp_w={}&unit=1&submit=Retrieve+Data&de=0&format=3&line_out=0&remove_js=on&en_unit=0&output=0&bibrefs=1&page_size=15&show_obs_wl=1&show_calc_wl=1&unc_out=1&order_out=0&max_low_enrg=&show_av=2&max_upp_enrg=&tsb_value=0&min_str=&A_out=1&intens_out=on&max_str=&allowed_out=1&forbid_out=1&min_accur=&min_intens=&conf_out=on&term_out=on&enrg_out=on&J_out=on"
+        site = "https://physics.nist.gov/cgi-bin/ASD/lines1.pl?spectra={}" \
+               "&limits_type=0&low_w={}" \
+               "&upp_w={}" \
+               "&unit=1&submit=Retrieve+Data&de=0&format=3&line_out=0&remove_js=on&en_unit=0&output=0&bibrefs=1&page_size=15&show_obs_wl=1&show_calc_wl=1&unc_out=1&order_out=0&max_low_enrg=&show_av=2&max_upp_enrg=&tsb_value=0&min_str=&A_out=1&intens_out=on&max_str=&allowed_out=1&forbid_out=1&min_accur=&min_intens=&conf_out=on&term_out=on&enrg_out=on&J_out=on"
+
         site = site.format(self.element, self.low_w, self.upper_w)
         respond = requests.get(site)
         soup = BeautifulSoup(respond.content, 'lxml')
         html_data = soup.get_text()
         html_data = html_data.replace('"', "")
         data = io.StringIO(html_data)
-        self.data_frame = pd.read_csv(data, sep="\t")
+        self.data_frame = pd.read_csv(data, sep="\t").drop('Unnamed: 20',axis=1)
+        print(self.data_frame.columns)
 
     def clean_intensity(self):
 
-        for i in range(len(self.data_frame['intens'])):
-            self.data_frame['intens'].iloc[i] = re.sub('[^0-9]', '', str(self.data_frame['intens'].iloc[i]))
-
+        self.data_frame['intens'] = self.data_frame['intens'].apply(lambda item: re.sub('[^0-9]', '', str(item)))
         self.data_frame = self.data_frame[self.data_frame['intens'] != '']
         self.data_frame['intens'] = pd.to_numeric(self.data_frame['intens'])
 
@@ -68,15 +68,21 @@ class Lines_LIBS(object):
         else:
             self.data_frame = self.data_frame[['obs_wl_air(nm)', 'intens', 'gA(s^-1)']]
 
-    def filter_line(self, line, count):
+    def filter_sp(self, sp_num):
+
+        self.data_frame = self.data_frame[self.data_frame['sp_num'].isin(sp_num)]
+
+    def search_nearest_lines(self, line, count):
+        sorted_by_lines_df = self.data_frame.iloc[(self.data_frame['obs_wl_air(nm)'] - line).abs().argsort()]
+        sorted_by_lines_df = sorted_by_lines_df[sorted_by_lines_df['intens'].notna()]
+        sorted_by_intens_df = sorted_by_lines_df[:5].sort_values(by=['intens'], ascending=False)
+        print(sorted_by_intens_df)
+
         return pd.DataFrame(
             self.data_frame.iloc[(self.data_frame['obs_wl_air(nm)'] - line).abs().argsort()[:count]].sort_values(
                 by=['intens'], ascending=False).iloc[0]).transpose()
 
-    def filter_sp(self, sp=1):
-        self.data_frame = self.data_frame[self.data_frame['sp_num'] == 1]
-
 
 if __name__ == '__main__':
-    line = Lines_LIBS('Ar', 200, 940, strongLines=False, first_sp=False)
-    print(line.data_frame)
+    line = NIST_line('Ar', 200, 940, strongLines=False, sp_num= [1,2,4])
+    line.search_nearest_lines(201, 5)
